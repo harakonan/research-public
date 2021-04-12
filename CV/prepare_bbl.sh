8cd1dc49c6e9d82792cb3c134b7cbda987d094d8
harakonan <hara.konan@gmail.com>
2021-04-11 22:12:45 -0700

    [UPDATE] CV

--- CV/prepare_bbl.sh
+++ CV/prepare_bbl.sh
@@ -5,7 +5,7 @@ bibtex eco
 bibtex med
 
 # underline the owner's name in .bbl
-sed -i 's/Konan\ Hara/\\textbf\{\\underline\{Konan\ Hara\}\}/g' eco.bbl
-sed -e ':a' -e 'N' -e '$!ba' -i -e 's/Konan\n\ \ Hara,/\\textbf\{\\underline\{Konan\ Hara\}\},\n\ /g' eco.bbl
-sed -i 's/Konan\ Hara/\\textbf\{\\underline\{Konan\ Hara\}\}/g' med.bbl
-sed -e ':a' -e 'N' -e '$!ba' -i -e 's/Konan\n\ \ Hara,/\\textbf\{\\underline\{Konan\ Hara\}\},\n\ /g' med.bbl
+sed -i '' -e 's/Konan\ Hara/\\textbf\{\\underline\{Konan\ Hara\}\}/g' eco.bbl
+sed -e ':a' -e 'N' -e '$!ba' -i '' -e 's/Konan\n\ \ Hara,/\\textbf\{\\underline\{Konan\ Hara\}\},\n\ /g' eco.bbl
+sed -i '' -e 's/Konan\ Hara/\\textbf\{\\underline\{Konan\ Hara\}\}/g' med.bbl
+sed -e ':a' -e 'N' -e '$!ba' -i '' -e 's/Konan\n\ \ Hara,/\\textbf\{\\underline\{Konan\ Hara\}\},\n\ /g' med.bbl
