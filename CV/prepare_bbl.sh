3c3423c526590e18e2e0dd2d08a956c022d47855
harakonan <hara.konan@gmail.com>
2021-12-27 04:08:01 -0700

    [UPDATE] CV notes

--- CV/prepare_bbl.sh
+++ CV/prepare_bbl.sh
@@ -2,10 +2,13 @@
 
 # create .bbl
 bibtex eco
-bibtex med
+bibtex medpri
+bibtex medoth
 
 # underline the owner's name in .bbl
 sed -i '' -e 's/Konan\ Hara/\\textbf\{\\underline\{Konan\ Hara\}\}/g' eco.bbl
 sed -e ':a' -e 'N' -e '$!ba' -i '' -e 's/Konan\n\ \ Hara,/\\textbf\{\\underline\{Konan\ Hara\}\},\n\ /g' eco.bbl
-sed -i '' -e 's/Konan\ Hara/\\textbf\{\\underline\{Konan\ Hara\}\}/g' med.bbl
-sed -e ':a' -e 'N' -e '$!ba' -i '' -e 's/Konan\n\ \ Hara,/\\textbf\{\\underline\{Konan\ Hara\}\},\n\ /g' med.bbl
+sed -i '' -e 's/Konan\ Hara/\\textbf\{\\underline\{Konan\ Hara\}\}/g' medpri.bbl
+sed -e ':a' -e 'N' -e '$!ba' -i '' -e 's/Konan\n\ \ Hara,/\\textbf\{\\underline\{Konan\ Hara\}\},\n\ /g' medpri.bbl
+sed -i '' -e 's/Konan\ Hara/\\textbf\{\\underline\{Konan\ Hara\}\}/g' medoth.bbl
+sed -e ':a' -e 'N' -e '$!ba' -i '' -e 's/Konan\n\ \ Hara,/\\textbf\{\\underline\{Konan\ Hara\}\},\n\ /g' medoth.bbl
